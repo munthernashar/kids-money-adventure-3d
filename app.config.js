929cc8345546e13328ca6dc922d8bb75f4ad72b9
munthernashar <munther.nashar@nashcon.de>
2026-09-09 00:39:28 +0400

    fix: remove asset refs, add app.config.js for EAS Build

---
+++ app.config.js
@@ -0,0 +1,25 @@
+export default {
+  expo: {
+    name: "Geld-Abenteuer",
+    slug: "kids-money-adventure-3d",
+    version: "1.0.0",
+    orientation: "portrait",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.munthernashar.kidsmoneyadventure",
+      buildConfiguration: {
+        simple: true
+      }
+    },
+    android: {
+      package: "com.munthernashar.kidsmoneyadventure",
+      adaptiveIcon: {
+        foregroundImage: undefined,
+        backgroundColor: "#ffffff"
+      }
+    }
+  }
+};
